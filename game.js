feaca96604647790f72fd42ca1464d1b20c595af
John Daniel <daniel@bosser.com>
2014-12-25 22:25:43 +0100

    ball.setVelocity, correct redraw of ball and remove uneccesary calculation
    ball.setVelocity() will change the speed of the ball in real time.
    
    When we drew the ball we drew with uneven pixels. I round the positions and dimensions to whole numbers and draw the ball
    
    When we hit a wall, we do not need to recalculate K. Instead, we just flip either dX or dY (depending on the which wall we hit).

--- game.js
+++ game.js
@@ -17,14 +17,13 @@ canvas.width = width;
 
 var ctx = canvas.getContext("2d");
 
-//Draw background
-ctx.fillStyle = backgroundColor;
+//Draw backgroucanvasDrawerctx.fillStyle = backgroundColor;
 ctx.fillRect(0,0,canvas.width, canvas.height);
 
 var Ball = function(height, width, startX, startY, boundX, boundY){
     this.color = elementColor;
-    this.height = height;
-    this.width = width;
+    this.height = parseInt(height);
+    this.width = parseInt(width);
     
     //Where is the ball?
     this.x = startX;
@@ -52,13 +51,13 @@ var Ball = function(height, width, startX, startY, boundX, boundY){
 
 Ball.prototype.draw = function(canvasDrawer){
     //first clear the ball
-    ctx.fillStyle = backgroundColor;
-    ctx.clearRect(ball.oldX, ball.oldY, ball.width, ball.height);
-    ctx.fillRect(ball.oldX - 2, ball.oldY -2, ball.width +4, ball.height + 4); //Someone needs to take a look into this. Why doen't the ball get cleared when we only use oldX and oldY? 
+    canvasDrawer.fillStyle = backgroundColor;
+    //canvasDrawer.clearRect(ball.oldX, ball.oldY, ball.width, ball.height);
+    canvasDrawer.fillRect(parseInt(ball.oldX), parseInt(ball.oldY), ball.width, ball.height);
     
     //Now, draw a new ball :D
     canvasDrawer.fillStyle = this.color;
-    canvasDrawer.fillRect(this.x, this.y, this.width, this.height);
+    canvasDrawer.fillRect(parseInt(this.x), parseInt(this.y), this.width, this.height);
 };
 Ball.prototype.calculatedXdY = function(){
     var v = this.velocity;
@@ -71,25 +70,85 @@ Ball.prototype.calculatedXdY = function(){
     this.dX = calcdX;
     this.dY = calcdY;
 };
+Ball.prototype.setXfromCenter = function(center){
+    this.x = center - this.width/2;
+    return this.x;
+}
+Ball.prototype.setYfromCenter = function(center){
+    this.y = center - this.height/2;
+    return this.y;
+}
 Ball.prototype.move = function(){
+    
+    var checkHits = function(ball){
+        
+        var setRight = function(inVal, bound){
+            
+            var outside = inVal - bound;
+            var result = bound - outside;
+            return result;
+        }
+
+        //Vertical walls
+        if(ball.centerX >= ball.boundX || ball.centerX < 0){
+
+            //set position right
+            
+            if(ball.centerX > ball.boundX) { 
+                 ball.setXfromCenter(setRight(ball.centerX, ball.boundX));
+            }
+            if(ball.centerX < 0) {
+                ball.setXfromCenter(-ball.centerX);
+            }
+                
+            //Bounce, go other direction
+            ball.right = ball.right * -1;
+            ball.dX = ball.dX * ball.right;
+        };
+        
+        //Horizontal walls
+        if(ball.centerY >= ball.boundY || ball.centerY <= 0){
+
+            //set position right
+            if(ball.centerY > ball.boundY){ 
+                ball.setYfromCenter(setRight(ball.centerY, ball.boundY));
+            }
+            if(ball.centerY < 0){
+                ball.setYfromCenter(-ball.centerY);
+            }
+
+            //Flip k, (but that is the same as a flip of dY
+            ball.dY = -ball.dY;
+        };
+        
+        //TODO: Check for a hit with platform
+    }
     if (!this.dX || !this.dY) this.calculatedXdY();
     
     this.oldX = this.x;
-    this.oldY = this.y;
+    this.oldY = this.y;    
     
+    //Check for wall hits
     
-    if(/*this.x >= this.boundX || this.x <= 0 || */this.y >= this.boundY || this.y <= 0){
-        //We hit left or right wall!! D:
-        if (this.x >= this.boundX || this.x <= 0) this.right = this.right * -1; //Change direction
-        this.k = this.k * -1;
-        this.calculatedXdY();
-    }
+    checkHits(this);
     
     this.x = this.x + this.dX;
     this.y = this.y + this.dY;
     
     this.centerX = this.x + this.width/2;
+    this.centerY = this.y + this.height/2;
     
+};
+Ball.prototype.setVelocity = function(vel){
+    //Recalculate dX and dY. They are proportional to velocity
+    var change = vel/this.velocity;
+    this.dX = this.dX * change;
+    this.dY = this.dY * change;
+    this.velocity = vel;
+};
+Ball.prototype.setK = function(inK){
+    this.k = inK;
+    this.calculatedXdY();
 };
 var start = null;
 var ball = new Ball(10, 10, width/2 - 5, height/2-5, canvas.width, canvas.height);
@@ -100,13 +159,18 @@ var step = function(timestamp){
     ball.move();
     
     if(running) window.requestAnimationFrame(step);
-    if(ball.x <= 0){
-        alert("Lefty vann!");
-        running = false;
-    }
-    if(ball.x >= ball.boundX){
+    
+    //Very advanced system for detection of winner. Please be careful when you
+    //uncomment this. It could lead to a broken toe, or worse, a 
+    //pinple on your elbow. Please uncomment with cuation. 
+    
+    /*if(ball.centerX <= 0){
         alert("alliansen vann!")
         running = false;
     }
+    if(ball.centerX >= ball.boundX){
+        alert("Lefty vann!");
+        running = false;
+    }*/
 };
 window.requestAnimationFrame(step);
